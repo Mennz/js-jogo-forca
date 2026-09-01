f8a7247d3ef1043b435901b0eafba179f5d4275f
Mennz <davi.mendoza.rezende.c@gmail.com>
2026-09-01 11:03:36 -0300

    le a letra do usuario em loop

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const readline = require("readline");
+
 const palavras = [
   "abacaxi",
   "computador",
@@ -13,6 +15,7 @@ const indice = Math.floor(Math.random() * palavras.length);
 const palavraEscolhida = palavras[indice];
 
 const letrasCertas = palavraEscolhida.split("").map(() => false);
+const letrasTentadas = [];
 
 function montarPalavraEscondida() {
   return palavraEscolhida
@@ -21,4 +24,35 @@ function montarPalavraEscondida() {
     .join(" ");
 }
 
-console.log(montarPalavraEscondida());
+function palavraCompleta() {
+  return letrasCertas.every((certa) => certa);
+}
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+function perguntarLetra() {
+  console.log("\n" + montarPalavraEscondida());
+  rl.question("Digite uma letra: ", (resposta) => {
+    const letra = resposta.toLowerCase();
+
+    if (letrasTentadas.includes(letra)) {
+      console.log("voce ja tentou essa letra");
+      perguntarLetra();
+      return;
+    }
+    letrasTentadas.push(letra);
+
+    palavraEscolhida.split("").forEach((letraDaPalavra, i) => {
+      if (letraDaPalavra === letra) letrasCertas[i] = true;
+    });
+
+    if (palavraCompleta()) {
+      console.log("\nvoce acertou: " + palavraEscolhida);
+      rl.close();
+    } else {
+      perguntarLetra();
+    }
+  });
+}
+
+perguntarLetra();

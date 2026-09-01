3c0f265388eb2083737917c7fcba7b9ba19de9ff
Mennz <davi.mendoza.rezende.c@gmail.com>
2026-09-01 11:01:33 -0300

    lista de palavras e sorteio

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("jogo da forca");
+const palavras = [
+  "abacaxi",
+  "computador",
+  "javascript",
+  "montanha",
+  "elefante",
+  "biblioteca",
+  "guitarra",
+  "foguete",
+];
+
+const indice = Math.floor(Math.random() * palavras.length);
+const palavraEscolhida = palavras[indice];
+
+console.log(palavraEscolhida);
